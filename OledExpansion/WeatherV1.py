5cbe982abe549901fd2c069bea96810436e75c5c
bjamshid <bjamshid@users.noreply.github.com>
2021-06-08 23:50:04 -0400

    Update WeatherV1.py

--- OledExpansion/WeatherV1.py
+++ OledExpansion/WeatherV1.py
@@ -6,7 +6,7 @@ api_key = <API_KEY>
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
 city_name = <CITY_NAME>
 complete_url = base_url + "appid=" + api_key + "&q=" + city_name + "&units=metric"
-os.system("oled-exp -i > /dev/null")
+os.system("oled-exp -i")
 while True:
     response = requests.get(complete_url)
     x = response.json()
@@ -20,16 +20,17 @@ while True:
         z = x["weather"]
         weather_description = z[0]["description"]
         os.system("oled-exp -c > /dev/null")
-        os.system("oled-exp cursor 1,0 > /dev/null")
+        os.system("oled-exp cursor 0,0 > /dev/null")
         os.system("oled-exp write 'Temp: {}' > /dev/null".format(str(current_temperature)[:4] + " C"))
-        os.system("oled-exp cursor 2,0 > /dev/null")
+        os.system("oled-exp cursor 1,0 > /dev/null")
         os.system("oled-exp write 'Feels: {}' > /dev/null".format(str(feels)[:4] + " C"))
         os.system("oled-exp cursor 3,0 > /dev/null")
         os.system("oled-exp write 'Humidity: {}' > /dev/null".format(str(current_humidity) + "%"))
         os.system("oled-exp cursor 4,0 > /dev/null")
         os.system("oled-exp write 'Description: {}' > /dev/null".format(str(weather_description).title()))
-        os.system("oled-exp cursor 6,0 > /dev/null")
+        os.system("oled-exp cursor 7,0 > /dev/null")
         os.system("oled-exp write '{}' > /dev/null".format(now.strftime("%Y/%b/%d %H:%M:%S")))
     else:
         os.system("oled-exp write ERROR! > /dev/null")
-    sleep(2700)
+    sleep(180)
+
